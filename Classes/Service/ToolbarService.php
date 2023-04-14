c4589af34dce6cfe9da1b6636f2dda9c3d1c23d0
Klaus HÃrmann-Engl <Klaus.Hoermann-Engl@world-direct.at>
2023-04-15 00:55:12 +0200

    task: adapted builfinfo for other installations

--- Classes/Service/ToolbarService.php
+++ Classes/Service/ToolbarService.php
@@ -2,12 +2,10 @@
 
 namespace WorldDirect\Buildinfo\Service;
 
-use Symfony\Component\Filesystem\Exception\FileNotFoundException;
-use TYPO3\CMS\Backend\Backend\Event\SystemInformationToolbarCollectorEvent;
-use TYPO3\CMS\Backend\Toolbar\Enumeration\InformationStatus;
-use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Localization\LanguageService;
-use WorldDirect\Buildinfo\Utility\BasicUtility;
+use WorldDirect\Buildinfo\Utility\BuildinfoUtility;
+use TYPO3\CMS\Backend\Toolbar\Enumeration\InformationStatus;
+use TYPO3\CMS\Backend\Backend\Event\SystemInformationToolbarCollectorEvent;
 
 /*
  * This file is part of the TYPO3 extension "worlddirect/buildinfo".
@@ -36,11 +34,11 @@ class ToolbarService
     const LANG_PREFIX = 'LLL:EXT:buildinfo/Resources/Private/Language/locallang_db.xlf:';
 
     /**
-     * Extension configuration
+     * The buildinfo utility.
      *
-     * @var array<string>
+     * @var BuildinfoUtility
      */
-    protected $extConf = null;
+    protected $buildinfoUtility = null;
 
     /**
      * Language service
@@ -50,27 +48,19 @@ class ToolbarService
     protected $languageService = null;
 
     /**
-     * The project path
-     *
-     * @var string
-     */
-    protected $projectPath = '';
-
-    /**
      * Constructor for new ToolbarService objects
      */
-    public function __construct()
+    public function __construct(BuildinfoUtility $buildinfoUtility)
     {
+        $this->buildinfoUtility = $buildinfoUtility;
         $this->languageService = $GLOBALS['LANG'];
-        $this->extConf = BasicUtility::getConfiguration('tx_buildinfo');
-        $this->projectPath = Environment::getProjectPath() . '/';
     }
 
     /**
      * Adds a certain information from a file to the SystemInformations toolbar.
      *
      * @param SystemInformationToolbarCollectorEvent $event The system information toolbar event
-     * @param string $type Which type is it?
+     * @param string $type Which type is it? "buildnumber", "
      * @param string $icon The desired icon to add
      * @param string $infoStatus The desired info status type
      *
@@ -78,47 +68,17 @@ class ToolbarService
      */
     public function addFileContentToSystemInformation(SystemInformationToolbarCollectorEvent $event, string $type, string $icon, string $infoStatus = InformationStatus::STATUS_INFO):void
     {
-        $file = $type . 'File';
-        if (isset($this->extConf[$file]) && $this->extConf[$file] != '') {
-            $infoFile = $this->projectPath . $this->extConf[$file];
-            if (file_exists($infoFile) && !empty($infoFile)) {
-                $event->getToolbarItem()->addSystemInformation(
-                    $this->languageService->sL(self::LANG_PREFIX . 'buildinfo.' . $type . '.title'),
-                    $this->getFormattedFileContent($infoFile, $type),
-                    $icon,
-                    $infoStatus
-                );
-            }
-        }
-    }
+        // Get the info content for the specific type (timestamp, gitversion, ...)
+        $infoContent = $this->buildinfoUtility->getFileContent($type);
 
-    /**
-     * Function gets the file and the type of the information.
-     * Depending on the type of information the content of file a formatting of this content
-     * may happen.
-     *
-     * @param string $file The file with the content to be formatted
-     * @param string $type The type of information
-     *
-     * @return string The formatted content of the file
-     */
-    private function getFormattedFileContent(string $file, string $type)
-    {
-        switch ($type) {
-            case 'buildTimestamp':
-                $timestamp = '';
-                $fileContent = file_get_contents($file);
-                if ($fileContent) {
-                    $timestamp = $fileContent;
-                }
-                return BasicUtility::formatTimestamp(intval($timestamp));
-            default:
-                $fileContent = file_get_contents($file);
-                if ($fileContent) {
-                    return $fileContent;
-                } else {
-                    throw new FileNotFoundException();
-                }
+        // IF the read content is not empty, add it to the toolbar items
+        if ($infoContent != '') {
+            $event->getToolbarItem()->addSystemInformation(
+                $this->languageService->sL(self::LANG_PREFIX . 'buildinfo.' . $type . '.title'),
+                $infoContent,
+                $icon,
+                $infoStatus
+            );
         }
     }
 }
--- Classes/Utility/BuildinfoUtility.php
+++ Classes/Utility/BuildinfoUtility.php
@@ -2,6 +2,10 @@
 
 namespace WorldDirect\Buildinfo\Utility;
 
+use TYPO3\CMS\Core\Core\Environment;
+use WorldDirect\Buildinfo\Utility\BasicUtility;
+use TYPO3\CMS\Core\Localization\Exception\FileNotFoundException;
+
 /*
  * This file is part of the TYPO3 extension "worlddirect/buildinfo".
  *
@@ -23,4 +27,83 @@ namespace WorldDirect\Buildinfo\Utility;
  */
 class BuildinfoUtility
 {
+    /**
+     * Extension configuration
+     *
+     * @var array<string>
+     */
+    protected $extConf = null;
+
+    /**
+     * The project path
+     *
+     * @var string
+     */
+    protected $projectPath = '';
+
+    /**
+     * Constructor for new BuildinfoUtility objects.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->extConf = BasicUtility::getConfiguration('tx_buildinfo');
+        $this->projectPath = Environment::getProjectPath() . '/';
+    }
+
+    /**
+     * Method returns the file content of a certain type. The function takes a look
+     * into the configuration and reads the correct file from disc and return the
+     * value.
+     *
+     * @param string $type The desired typ
+     *
+     * @return string The type file content
+     */
+    public function getFileContent(string $type): string
+    {
+        $file = $type . 'File';
+
+        // Check if the TypoScript settings exists
+        if (isset($this->extConf[$file]) && $this->extConf[$file] != '') {
+            $infoFile = $this->projectPath . $this->extConf[$file];
+            if (file_exists($infoFile) && !empty($infoFile)) {
+                return $this->getFormattedFileContent($infoFile, $type);
+            }
+        }
+
+        // The typoscript settings are not set, therefore return empty
+        return '';
+    }
+
+    /**
+     * Function gets the file and the type of the information.
+     * Depending on the type of information the content of file a formatting of this content
+     * may happen.
+     *
+     * @param string $file The file with the content to be formatted
+     * @param string $type The type of information
+     *
+     * @return string The formatted content of the file
+     */
+    private function getFormattedFileContent(string $file, string $type)
+    {
+        switch ($type) {
+            case 'buildTimestamp':
+                $timestamp = '';
+                $fileContent = file_get_contents($file);
+                if ($fileContent) {
+                    $timestamp = $fileContent;
+                }
+                return BasicUtility::formatTimestamp(intval($timestamp));
+            default:
+                $fileContent = file_get_contents($file);
+                if ($fileContent) {
+                    return $fileContent;
+                } else {
+                    throw new FileNotFoundException();
+                }
+        }
+    }
 }
